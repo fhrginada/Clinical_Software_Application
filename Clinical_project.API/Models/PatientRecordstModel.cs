c37b74234bf5d932dfd8c76b4036fdd105152b2d
Nada Hassan <nadah8120@gmail.com>
2025-10-17 19:03:29 +0300

    add patientRecordModel

---
+++ Clinical_project.API/Models/PatientRecordstModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Clinical_Project.Models
+{
+    internal class PatientRecordsModel
+    {
+        [Key]
+        public int RecordID { get; set; }
+
+        [Required]
+        
+        public int PatientID { get; set; }
+
+        [Required]
+        public int DoctorID { get; set; }
+
+        public string DataRecorded { get; set; }
+        public string MedicalHistory { get; set; }
+        public string Diagnosis { get; set; }
+        public string Treatment { get; set; }
+        public DateTime? FollowUpDate
+        {
+            get; set;
+        }
+        [Url]
+        public  string AttachtmentURL { get; set; }
+    }
+}
---
+++ Clinical_project/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Clinical_Project
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
