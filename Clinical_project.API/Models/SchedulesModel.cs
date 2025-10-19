3722f0c1258e2d0a3e0a324498cea7aa2edf0ef6
Nada Hassan <nadah8120@gmail.com>
2025-10-19 21:27:11 +0300

    added Schedules& Medications Models

---
+++ Clinical_project.API/Models/SchedulesModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Clinical_project.API.Models
+{
+    public class SchedulesModel
+    {
+        public int ScheduleId { get; set; }
+        public int DoctorID { get; set; }
+        public DateTime AvalibleDate { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        
+        public String? SlotDuration { get; set; }
+
+    }
+}
--- Clinical_project.API/obj/Debug/net8.0/Clinical_project.API.AssemblyInfo.cs
+++ Clinical_project.API/obj/Debug/net8.0/Clinical_project.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Clinical_project.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c37b74234bf5d932dfd8c76b4036fdd105152b2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clinical_project.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Clinical_project.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
